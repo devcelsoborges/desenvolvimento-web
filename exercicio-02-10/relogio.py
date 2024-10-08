61ef1e1c8e565742947b6bd685f0db8286de7bee
devcelsoborges <borgesnetocs@gmail.com>
2024-10-08 15:56:59 -0300

    Update

---
+++ exercicio-02-10/relogio.py
@@ -0,0 +1,49 @@
+﻿from datetime import time, datetime;
+
+from tkinter import *
+import tkinter
+from datetime import datetime, time
+
+##############################  Cores   ##############################
+cor1 = "#3d3d3d"  # Preto
+cor2 = "#fafcff"  # Branco
+cor3 = "#21c25c"  # Verde
+cor4 = "#eb463b"  # Vermelho
+cor5 = "#dedcdc"  # Cinza
+cor6 = "#3080f0"  # Azul
+
+fundo = cor1
+cor = cor4
+
+janela=Tk()
+janela.title("")
+janela.geometry("600x200")
+janela.resizable(width=False, height=False)
+janela.configure(bg=cor1)
+
+
+######################################################################
+
+
+
+
+
+def relogio():
+
+    tempo = datetime.now()
+    horarios = datetime.now().strftime("%H:%M:%S")
+    hora = tempo.strftime("%H:%M:%S")
+
+    l1.config(text=hora)
+    l1.after(200, relogio)
+
+        
+
+l1 = Label(janela, text="", font=("Arial 110"), bg=fundo, fg=cor)
+l1.grid(row=0, column=0, sticky=NW, padx=5)
+
+relogio()
+janela.mainloop()
+
+
+    
---
+++ exercicio-02-10/teste.js
@@ -0,0 +1,19 @@
+﻿let titulo = document.getElementById('titulo')
+titulo.textContent = "Olá!"
+
+let botao = document.getElementById('botao')
+botao.textContent = "Botão"
+
+const frutas = ["Banana", "Maçã", "Laranja"]
+const lista = document.getElementById('lista')
+
+
+botao.addEventListener("click", function(){
+    titulo.textContent = "Mudou geral"
+    for(let i=0; i<frutas.length; i++){;
+    let item = document.createElement('li');
+    item.textContent = frutas[i];
+    lista.appendChild(item);
+    lista.classList.add("remove-ponto");
+    }
+});
